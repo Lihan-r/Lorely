9e22c6d15f38475cdade06a322e1c78540c8393a
Lihan-r <Rashidlihan@gmail.com>
2026-02-11 18:58:40 -0500

    Fix UserRepositoryTest to use SpringBootTest with full context

--- backend/src/test/java/com/lorely/repository/UserRepositoryTest.java
+++ backend/src/test/java/com/lorely/repository/UserRepositoryTest.java
@@ -1,21 +1,29 @@
 package com.lorely.repository;
 
 import com.lorely.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
 
     @Test
     void shouldSaveAndFindUserByEmail() {
